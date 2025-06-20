b3261e998e56fa89daa670d6afe3ef53e103ddb1
ankitsaini85 <nareshsaini85879@gmail.com>
2025-06-20 10:10:56 +0530

    frontend link added

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5174', // Update if needed
+    origin: 'https://polling-system-4wlvsa12q-ankit-sainis-projects-b6e2b568.vercel.app', 
     methods: ['GET', 'POST']
   }
 });
