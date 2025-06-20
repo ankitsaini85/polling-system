3006a543d447d419bb56a36fafbe07456abac6c6
ankitsaini85 <nareshsaini85879@gmail.com>
2025-06-20 10:33:28 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5174', 'https://polling-system-4wlvsa12q-ankit-sainis-projects-b6e2b568.vercel.app/'],
+    origin: ['http://localhost:5174', 'https://polling-system-3xey245hv-ankit-sainis-projects-b6e2b568.vercel.app/'],
     methods: ['GET', 'POST'],
     credentials: true
   },
